6590b84359f7d43a5b13d16c7e0312abc1b6aba6
David Parker <david.m.parker92@gmail.com>
2023-01-05 22:09:53 -0500

    started comment section

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,13 +1,22 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
-User.hasMany(Blog, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+// User.hasMany(Blog, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE',
+// });
 
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
 });
+// comment not being recognized
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id'
+});
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog, Comment };
