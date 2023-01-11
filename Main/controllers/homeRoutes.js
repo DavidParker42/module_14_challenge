b754b74141d3a290bc5e46bbc78a9ef456d3140e
David Parker <david.m.parker92@gmail.com>
2023-01-10 21:20:39 -0500

    commit changs

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -53,15 +53,15 @@ router.get('/blog/:id', async (req, res) => {
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Blog }],
-    });
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ['password'] },
+    //   include: [{ model: Blog }],
+    // });
 
-    const user = userData.get({ plain: true });
+    // const user = userData.get({ plain: true });
 
     res.render('profile', {
-      ...user,
+      // ...user,
       logged_in: true
     });
   } catch (err) {
